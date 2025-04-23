9207f89ec003a395f2882255160d1c5074bf7260
User30_14n <User30_14n@koop.teh>
2025-04-23 13:10:30 +0300

    Работа с json

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let getDataFromJSONPlaceHolder = async () => {
+   try{
+    let response = await fetch ("https://jsonplaceholder.typicode.com/todos/10")
+
+    if (!response.ok) console.log("Ошибка")
+
+    let data = await response.json()
+    console.log("Наши данные", data)
+   } catch (error){
+    console.log("Возникла ошибка при запросе", error)
+   } finally {
+    console.log("Запрос завершился)")
+   }
+}
+
+
